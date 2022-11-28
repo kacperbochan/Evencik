38d3954025b0d97137be11c5527fbd2d8794eb00
Kacper Bochan <kacperbochan@gmail.com>
2022-11-28 12:21:23 +0100

    update - edit v2

--- src/app/src/types/Zadanie.ts
+++ src/app/src/types/Zadanie.ts
@@ -7,14 +7,32 @@ export class Zadanie {
   private _wolontariuszeObecni: number;
   private _wolontariuszeWymagani: number;
 
-  constructor(nazwa:string, wymagani:number=-1, obecni:number=-1){
-      this._indeks = Zadanie.amount++;
+  constructor(nazwa:string, wymagani:number=-1, obecni:number=-1, cp:boolean=false, pocz:Date=new Date(0), kon = new Date(0)){
+      this._indeks = (cp)?0:Zadanie.amount++;
       this._nazwa = nazwa;
-      this._czasPoczatek.setDate(this._czasPoczatek.getDate()+Math.random()*100%50);
-      this._czasKoniec.setDate(this._czasPoczatek.getDate()+Math.random()*10%3);
+
+      if(cp)
+        this._czasPoczatek = new Date(pocz);
+      else
+        this._czasPoczatek.setDate(this._czasPoczatek.getDate()+Math.random()*100%50);
+
+      if(cp)
+        this._czasKoniec = new Date(kon);
+      else
+        this._czasKoniec.setDate(this._czasPoczatek.getDate()+Math.random()*10%3);
+
       this._wolontariuszeWymagani = (wymagani>0)?wymagani:Math.floor(Math.random()*10%10);
       this._wolontariuszeObecni = (obecni>0)?obecni:Math.floor(Math.random()*100%this._wolontariuszeWymagani);
 
+  }
+
+  public copy(zad:Zadanie){
+    this._indeks = zad.indeks;
+    this._nazwa = zad.nazwa;
+    this._czasPoczatek = zad.czasPoczatek;
+    this._czasKoniec = zad.czasKoniec;
+    this._wolontariuszeObecni = zad.wolontariuszeObecni;
+    this._wolontariuszeWymagani = zad.wolontariuszeWymagani;
   }
 
   public get indeks() : number{
